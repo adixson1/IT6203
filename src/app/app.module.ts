e4f5eeea00d1bd368cb5c8cbf0022ca736b01635
Ayara Immari Dixson <ayara@Ayaras-Air.lan>
2022-10-12 11:43:23 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientInfoComponent } from './patient-info/patient-info.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { DoctorInfoComponent } from './doctor-info/doctor-info.component';
+import { SicknessDiseaseInfoComponent } from './sickness-disease-info/sickness-disease-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientInfoComponent,
+    AppointmentComponent,
+    DoctorInfoComponent,
+    SicknessDiseaseInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sickness-disease-info/sickness-disease-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sickness-disease-info',
+  templateUrl: './sickness-disease-info.component.html',
+  styleUrls: ['./sickness-disease-info.component.css']
+})
+export class SicknessDiseaseInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
